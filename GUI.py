8c77592bdd945206ce17b3731f0598a24de46f6f
Andy Munoz <56176735+amunoz124@users.noreply.github.com>
2023-09-22 20:28:45 -0700

    Updated GUI.py

--- GUI.py
+++ GUI.py
@@ -1,47 +1,67 @@
-import tkinter
+import tkinter as tk
 import requests
-import json
-from PIL import Image
+from PIL import Image, ImageTk
 import webbrowser
+import config
 
-root = tkinter.Tk()
-root.title('NASA\'s POD')
-root.config(bg='DodgerBlue2')
+class NasasPODApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("NASA's POD")
+        self.root.config(bg='DodgerBlue2')
+        self.api_key = config.api_key
+        self.load_data()
 
-#add your own API in your_API_key
-nasa_api = requests.get(your_API_key)
-api = json.loads(nasa_api.content)
-label1 = tkinter.Label(root, text=api['title'], font=('Harlow Solid Italic', 50))
-label1.config(bg='DodgerBlue2')  
-label1.pack()   
+    def load_data(self):
+        try:
+            nasa_api = requests.get(self.api_key)
+            api = nasa_api.json()
+            
+            self.display_title(api['title'])
+            self.display_date(api['date'])
+            self.display_explanation(api['explanation'])
 
-label2 = tkinter.Label(root, text='Date: ' + api['date'], font=('Arial', 16))
-label2.config(bg='DodgerBlue2')
-label2.pack()
+            image_url = api['hdurl']
+            self.resize_and_save_image(image_url, 'resized_image.png', (600, 380))
+            self.display_image('resized_image.png')
+        except Exception as e:
+            url = api.get('url', '')
+            self.display_error(url)
 
-label5 = tkinter.Text(root, font=('Arial'),width=75, height=10)
-label5.config(state="normal",)
-label5.insert(tkinter.INSERT,api['explanation'])
-label5.config(state="disabled")
-label5.config(bg='DodgerBlue4',fg='old lace')
-label5.pack()
+    def display_title(self, title):
+        label1 = tk.Label(self.root, text=title, font=('Harlow Solid Italic', 50), bg='DodgerBlue2')
+        label1.pack()
 
-try:
-    image_url = api['hdurl']
-    req_image = requests.get(image_url, stream=True).raw
-    image = Image.open(req_image)
-    image = image.resize((600,380), Image.ANTIALIAS)
-    image.save('image_url.png')
-    photo = tkinter.PhotoImage(file='image_url.png')
+    def display_date(self, date):
+        label2 = tk.Label(self.root, text='Date: ' + date, font=('Arial', 16), bg='DodgerBlue2')
+        label2.pack()
 
-    label3 = tkinter.Label(root, image=photo)
-    label3.pack()
-except Exception as e:
-    url = api ['url']
-    label4 = tkinter.Label(root, text=url, font=('Arial', 18))
-    label4.config(bg='DodgerBlue4', fg='old lace')
-    label4.pack()
-    butt = tkinter.Button(root,text='Redirect to website.', command=lambda:webbrowser.open(url, new=1))
-    butt.pack()
+    def display_explanation(self, explanation):
+        label5 = tk.Text(self.root, font=('Arial'), width=75, height=10, state="normal")
+        label5.insert(tk.END, explanation)
+        label5.config(state="disabled", bg='DodgerBlue4', fg='old lace')
+        label5.pack()
 
-root.mainloop()
+    def resize_and_save_image(self, image_url, save_path, size):
+        req_image = requests.get(image_url, stream=True).raw
+        image = Image.open(req_image)
+        image.thumbnail(size)
+        image.save(save_path)
+
+    def display_image(self, image_path):
+        photo = ImageTk.PhotoImage(file=image_path)
+        label3 = tk.Label(self.root, image=photo)
+        label3.image = photo
+        label3.pack()
+
+    def display_error(self, url):
+        label4 = tk.Label(self.root, text='Failed to fetch data', font=('Arial', 18), bg='DodgerBlue4', fg='old lace')
+        label4.pack()
+        if url:
+            butt = tk.Button(self.root, text='Redirect to website', command=lambda: webbrowser.open(url, new=1))
+            butt.pack()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = NasasPODApp(root)
+    root.mainloop()
